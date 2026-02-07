97079cea183d14bde3f9d2dd4b628929d5eb3119
Nestor <nestorpuentesin@gmail.com>
2026-02-07 16:11:34 -0600

    dandole forma a la plantilla, subiendo configuraciones basicas

---
+++ src/constant/images.ts
@@ -0,0 +1,11 @@
+import logo from "../assets/images/logo.png";
+import icon from "../assets/images/icon.png";
+// import logo from "../assets/images/logo_generico.png";
+// import icon from "../assets/images/logo_generico.png";
+// import bgAuth from "../assets/images/bg-auth.jpg";
+// import logoGPD from "../assets/images/logo-gpd.png";
+// import stamp from "../assets/images/sello-dif.png";
+// import cedula from "../assets/images/cedula-fiscal.jpg";
+
+const images = { logo, icon };
+export default images;
